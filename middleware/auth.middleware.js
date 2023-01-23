b6340ad8ac98560e4d3adb60ba4a62c06e7d342c
exile2003 <31631692+exile2003@users.noreply.github.com>
2023-01-24 01:30:25 +0200

    Add draw.io

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,9 +2,10 @@ const jwt = require('jsonwebtoken')
 const config = require('config')
 
 module.exports = (req, res, next) => {
+
     if (req.method === 'OPTIONS') { // OPTIONS это специальный метод, который присутствует в RestAPI, который
         // проверяет доступность сервера. Если это OPTIONS нам ничего лелать не нужно. Мы возвращаем next(), т. е.
-        // продолжаем делать запрос. Если это обычный запрос (post или get) vs будем обрабатывать его в блоке try catch.
+        // продолжаем делать запрос. Если это обычный запрос (post или get) мы будем обрабатывать его в блоке try catch.
         return next()
     }
 
